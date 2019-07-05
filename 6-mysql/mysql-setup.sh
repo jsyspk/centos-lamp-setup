14ef92bdd771dc996014407183f8adcfe1b993ac
Ali Masood <ahmad@zenex.co>
2019-07-05 18:48:22 +0900

    Updates the secure script

--- 6-mysql/mysql-setup.sh
+++ 6-mysql/mysql-setup.sh
@@ -17,4 +17,15 @@ echo "Install MySql" \
 && sudo spawn mysql_secure_installation \
 && expect \"Enter current password for root:\" \
 && send \"$MYSQL_PASSWORD\n\" \
+&& expect \"Change the password for root ?\" \
+&& send \"y\n\" \
+&& expect \"Remove anonymous users?\" \
+&& send \"y\n\" \
+&& expect \"Disallow root login remotely?\" \
+&& send \"y\n\" \
+&& expect \"Remove test database and access to it?\" \
+&& send \"y\n\" \
+&& expect \"Reload privilege tables now?\" \
+&& send \"y\n\" \
+&& echo "MySQL installation is complete" \
 && mysql -u root -p$MYSQL_PASSWORD
