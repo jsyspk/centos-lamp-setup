c290f67184b1e9fbc16f97b0654da5760a8bfc66
Ali Masood <ahmad@zenex.co>
2019-07-05 11:44:44 +0900

    Updates the php setup with php7.3

--- 4-php/php-setup-and-conf.sh
+++ 4-php/php-setup-and-conf.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
 echo "Get & install remi repo" \
 && sudo rpm -Uvh http://rpms.famillecollet.com/enterprise/remi-release-7.rpm \
+&& sudo rpm -Uvh wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm \
+&& echo "enable remi repo" \
+&& sudo yum install -y yum-utils \
+&& sudo yum-config-manager --enable remi-php73 \
 && echo "Install php" \
-&& sudo yum -y --enablerepo=remi,remi-php72 install php \
-&& sudo yum -y --enablerepo=remi,remi-php72 install php-fpm php-common \
+&& sudo yum install -y php php-fpm php-common \
 && echo "Install php packages" \
-&& sudo yum --enablerepo=remi,remi-php72 install -y php-opcache php-pecl-apcu php-cli php-pear php-pdo php-mysqlnd php-pgsql php-pecl-mongodb php-pecl-redis php-pecl-memcache php-pecl-memcached php-gd php-mbstring php-mcrypt php-xml php-zip php-imap \
+&& sudo yum install -y php-opcache php-pecl-apcu php-cli php-pear php-pdo php-mysqlnd php-pgsql php-pecl-mongodb php-pecl-redis php-pecl-memcache php-pecl-memcached php-gd php-mbstring php-mcrypt php-xml php-zip php-imap \
 && echo "Update php configuration" \
 && sudo sed -i 's/user = apache/user = nginx/g' /etc/php-fpm.d/www.conf \
 && sudo sed -i 's/group = apache/group = nginx/g' /etc/php-fpm.d/www.conf \
-&& sudo cp php.ini.dev /etc/php.ini \
+#&& sudo cp php.ini.dev /etc/php.ini \
 && echo "Start php-fpm" \
 && sudo systemctl start php-fpm \
 && sudo systemctl enable php-fpm
